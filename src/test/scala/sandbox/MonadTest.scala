83dd5a348b6a0514c3798b8565b693f6e44e2ee7
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-07-24 20:27:19 +1000

    Exercise 4.1.2 a monad is a functor

---
+++ src/test/scala/sandbox/MonadTest.scala
@@ -0,0 +1,15 @@
+package sandbox
+
+import sandbox.helper.Specification
+import scala.language.higherKinds
+
+class MonadTest extends Specification {
+
+  trait Monad[F[_]] {
+    def pure[A](a: A): F[A]
+
+    def flatMap[A, B](value: F[A])(func: A => F[B]): F[B]
+
+    def map[A, B](value: F[A])(func: A => B): F[B] = flatMap(value)(a => pure(func(a)))
+  }
+}
