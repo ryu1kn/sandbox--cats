cfc0c7a9032d92cdb2303e9cc274e149b437034d
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-07-25 17:53:08 +1000

    Exercise 4.3.1 Id

--- src/test/scala/sandbox/MonadTest.scala
+++ src/test/scala/sandbox/MonadTest.scala
@@ -1,15 +1,21 @@
 package sandbox
 
 import sandbox.helper.Specification
-import scala.language.higherKinds
 
 class MonadTest extends Specification {
-
-  trait Monad[F[_]] {
+  trait MyMonad[F[_]] {
     def pure[A](a: A): F[A]
 
     def flatMap[A, B](value: F[A])(func: A => F[B]): F[B]
 
     def map[A, B](value: F[A])(func: A => B): F[B] = flatMap(value)(a => pure(func(a)))
   }
+
+  import cats.Id
+
+  def pure[A](value: A): Id[A] = value
+
+  def map[A, B](initial: Id[A])(func: A => B): Id[B] = func(initial)
+
+  def flatMap[A, B](initial: Id[A])(func: A => Id[B]): Id[B] = func(initial)
 }
