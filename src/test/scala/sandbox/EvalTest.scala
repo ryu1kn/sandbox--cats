af312b22289dc66e3a2255cd18ca62b947cf7fec
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-07-25 19:28:11 +1000

    Exercise 4.6.5 Folding with Eval

---
+++ src/test/scala/sandbox/EvalTest.scala
@@ -0,0 +1,21 @@
+package sandbox
+
+import cats.Eval
+import sandbox.helper.Specification
+
+class EvalTest extends Specification {
+
+  "Eval" should {
+    def foldRightEval[A, B](as: List[A], acc: Eval[B])(fn: (A, Eval[B]) => Eval[B]): Eval[B] = as match {
+      case head :: tail => Eval.defer(fn(head, foldRightEval(tail, acc)(fn)))
+      case Nil => acc
+    }
+
+    def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B =
+      foldRightEval(as, Eval.now(acc)) { (a, b) => b.map(fn(a, _)) }.value
+
+    "be used to make foldRight stack-safe" in {
+      foldRight((1 to 100000).toList, 0L)(_ + _) shouldEqual 5000050000L
+    }
+  }
+}
