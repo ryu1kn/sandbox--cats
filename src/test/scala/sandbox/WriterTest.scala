95ae04d20b4f305c9c2db695af57a647ada53680
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-07-27 19:36:36 +1000

    Exercise 4.7.3 Using writer

--- src/test/scala/sandbox/WriterTest.scala
+++ src/test/scala/sandbox/WriterTest.scala
@@ -2,25 +2,38 @@ package sandbox
 
 import sandbox.helper.Specification
 
-import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
 import scala.concurrent.duration._
 
 class WriterTest extends Specification {
+
+  import cats.data.Writer
+  import cats.instances.vector._
+  import cats.syntax.applicative._
+  import cats.syntax.writer._
+
   def slowly[A](body: => A): A = try body finally Thread.sleep(100)
 
-  def factorial(n: Int): Int = {
-    val ans = slowly(if (n == 0) 1 else n * factorial(n - 1))
-    println(s"fact $n $ans")
-    ans
-  }
+  type Logged[A] = Writer[Vector[String], A]
+
+  def factorial(n: Int): Logged[Int] =
+    for {
+      ans <- slowly(if (n == 0) 1.pure[Logged] else factorial(n - 1).map(_ * n))
+      _ <- Vector(s"fact $n $ans").tell
+    } yield ans
 
   "Writer monad" should {
-    "WIP: accumulate logs" in {
-      Await.result(Future.sequence(Vector(
-        Future(factorial(3)),
-        Future(factorial(3))
-      )), 5.seconds)
+    "accumulate logs" in {
+      val Vector((logA, ansA), (logB, ansB)) =
+        Await.result(Future.sequence(Vector(
+          Future(factorial(3).run),
+          Future(factorial(5).run)
+        )), 5.seconds)
+      logA shouldEqual Vector("fact 0 1", "fact 1 1", "fact 2 2", "fact 3 6")
+      ansA shouldEqual 6
+      logB shouldEqual Vector("fact 0 1", "fact 1 1", "fact 2 2", "fact 3 6", "fact 4 24", "fact 5 120")
+      ansB shouldEqual 120
     }
   }
 }
