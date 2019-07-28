671f7e4c6b2fa4508640368b678e3a14ae59fafb
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-07-28 10:24:38 +1000

    Exercise 4.9.3-2 Evaluate whole expression by folding monads

--- src/test/scala/sandbox/StateTest.scala
+++ src/test/scala/sandbox/StateTest.scala
@@ -5,12 +5,25 @@ import sandbox.helper.Specification
 class StateTest extends Specification {
 
   import cats.data.State
+  import cats.syntax.applicative._
 
   type CalcState[A] = State[List[Int], A]
+
+  def evalInput(input: String): Int = {
+    val symbols = input.split(' ').toList
+    evalAll(symbols).runA(Nil).value
+  }
+
+  def evalAll(symbols: List[String]): CalcState[Int] =
+    symbols.foldLeft(0.pure[CalcState]) {
+      (step, symbol) => step.flatMap(_ => evalOne(symbol))
+    }
 
   def evalOne(sym: String): CalcState[Int] = sym match {
     case "+" => operator(_ + _)
     case "-" => operator(_ - _)
+    case "*" => operator(_ * _)
+    case "/" => operator(_ / _)
     case n => operand(n.toInt)
   }
 
@@ -44,6 +57,10 @@ class StateTest extends Specification {
       val (stack, answer) = programme.run(List[Int]()).value
       stack shouldEqual List()
       answer shouldEqual -1
+    }
+
+    "evaluate at one" in {
+      evalInput("1 2 + 3 4 + *") shouldEqual 21
     }
   }
 }
