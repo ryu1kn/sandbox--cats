9e78a06fa9eb5583106791b12962ce64eb22ac0c
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-07-28 20:38:58 +1000

    Exercise 4.10.1 Custom monad

---
+++ src/test/scala/sandbox/MonadTreeTest.scala
@@ -0,0 +1,40 @@
+package sandbox
+
+import sandbox.helper.Specification
+
+sealed trait Tree[+A]
+
+final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+final case class Leaf[A](value: A) extends Tree[A]
+
+class MonadTreeTest extends Specification {
+
+  def branch[A](left: Tree[A], right: Tree[A]): Tree[A] = Branch(left, right)
+  def leaf[A](value: A): Tree[A] = Leaf(value)
+
+  import cats.Monad
+
+  implicit val treeMonad = new Monad[Tree] {
+    override def pure[A](value: A): Tree[A] = leaf(value)
+
+    override def flatMap[A, B](fa: Tree[A])(f: A => Tree[B]): Tree[B] = fa match {
+      case Branch(left, right) => branch(flatMap(left)(f), flatMap(right)(f))
+      case Leaf(value) => f(value)
+    }
+
+    override def tailRecM[A, B](a: A)(f: A => Tree[Either[A, B]]): Tree[B] =
+      flatMap(f(a)) {
+        case Left(value) => tailRecM(value)(f)
+        case Right(value) => Leaf(value)
+      }
+  }
+
+  "Tree" should {
+    "become a monad" in {
+      import cats.syntax.flatMap._
+
+      branch(leaf(3), leaf(5)).flatMap(x => branch(leaf(x - 1), leaf(x + 1))) shouldEqual
+        branch(branch(leaf(2), leaf(4)), branch(leaf(4), leaf(6)))
+    }
+  }
+}
