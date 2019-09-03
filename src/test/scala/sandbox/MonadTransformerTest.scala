28842483a3b4035aabcf9aa9bded3827c80e90a3
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-09-03 19:18:17 +1000

    Exercise 5.4: Monad Transformer completed

--- src/test/scala/sandbox/MonadTransformerTest.scala
+++ src/test/scala/sandbox/MonadTransformerTest.scala
@@ -20,6 +20,19 @@ class MonadTransformerTest extends Specification {
       case None => EitherT.left(Future(s"$autobot is unreachable."))
     }
 
+  def canSpecialMove(ally1: String, ally2: String): Response[Boolean] =
+    for {
+      power1 <- getPowerLevel(ally1)
+      power2 <- getPowerLevel(ally2)
+    } yield power1 + power2 > 15
+
+  def tacticalReport(ally1: String, ally2: String): String =
+    Await.result(canSpecialMove(ally1, ally2).value, 1.second) match {
+      case Right(true) => s"$ally1 and $ally2 are ready to roll out!"
+      case Right(false) => "Jazz and Bumblebee need a recharge"
+      case Left(message) => s"Comms error: $message"
+    }
+
   val powerLevels = Map(
     "Jazz" -> 6,
     "Bumblebee" -> 8,
@@ -33,6 +46,22 @@ class MonadTransformerTest extends Specification {
 
     "return an error message if the autobot is unreadable" in {
       Await.result(getPowerLevel("Bumbleboo").value, 1.second) shouldEqual Left("Bumbleboo is unreachable.")
+    }
+
+    "tell if they can perform special move" in {
+      Await.result(canSpecialMove("Bumblebee", "Hot Rod").value, 1.second) shouldEqual Right(true)
+    }
+
+    "report that they can perform special move" in {
+      tacticalReport("Bumblebee", "Hot Rod") shouldEqual "Bumblebee and Hot Rod are ready to roll out!"
+    }
+
+    "report that they need a recharge" in {
+      tacticalReport("Jazz", "Bumblebee") shouldEqual "Jazz and Bumblebee need a recharge"
+    }
+
+    "report that unreachable autobot" in {
+      tacticalReport("Jazz", "Ironhide") shouldEqual "Comms error: Ironhide is unreachable."
     }
   }
 }
