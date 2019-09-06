9dc4dd25ea553ab262c50f7a285d7d65ae89a234
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-09-07 00:18:31 +1000

    Exercise 7.1: Foldable

---
+++ src/test/scala/sandbox/FoldTest.scala
@@ -0,0 +1,31 @@
+package sandbox
+
+import sandbox.helper.Specification
+
+class FoldTest extends Specification {
+  "foldLeft" should {
+    "join from left" in {
+      List(1, 2, 3).foldLeft(List.empty[Int]) { (a, i) => i :: a } shouldEqual List(3, 2, 1)
+    }
+  }
+
+  "foldRight" should {
+    "join from right" in {
+      List(1, 2, 3).foldRight(List.empty[Int]) { (i, a) => i :: a } shouldEqual List(1, 2, 3)
+    }
+  }
+
+  "foldLeft with cats" should {
+    import cats.Foldable
+
+    "fold list items from left" in {
+      import cats.instances.list._
+      Foldable[List].foldLeft(List(1, 2, 3), 0)(_ + _) shouldEqual 6
+    }
+
+    "fold option" in {
+      import cats.instances.option._
+      Foldable[Option].foldLeft(Option(1), 10)(_ + _) shouldEqual 11
+    }
+  }
+}
